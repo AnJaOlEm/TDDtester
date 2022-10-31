5dca1857494c5d5f166637a7b065a051600f2558
JariPii <89419494+JariPii@users.noreply.github.com>
2022-10-31 11:11:26 +0100

    Get users test green

--- src/test/java/org/example/repository/InMemoryUserRepositoryTest.java
+++ src/test/java/org/example/repository/InMemoryUserRepositoryTest.java
@@ -1,10 +1,14 @@
 package org.example.repository;
 
+import kotlin.reflect.jvm.internal.impl.metadata.ProtoBuf;
 import org.example.data.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 
@@ -26,6 +30,25 @@ class InMemoryUserRepositoryTest {
 
         Assertions.assertEquals(expected, users);
 
+    }
+
+    @ParameterizedTest
+    @MethodSource("userData")
+    void test_get_users(List<User> users, int expected) {
+
+        users.forEach(user -> userRepository.saveUser(user));
+
+        Assertions.assertEquals(expected, userRepository.getAllUsers().size());
+    }
+
+    static Stream<Arguments> userData() {
+        return Stream.of(
+                Arguments.arguments(List.of(
+                        new User("Kalle", "kallegmail", "123"),
+                        new User("Albin", "albingmail", "123"),
+                        new User("Stina", "stinagmail", "123")
+                ),3
+        ));
     }
 
 }
