9b8d2bc6c89a25a126dde00d74da8ef3fb6bc5eb
JariPii <89419494+JariPii@users.noreply.github.com>
2022-11-02 14:44:08 +0100

    get all blogs controller test passed
    last commit was fail test

--- src/main/java/org/example/controller/BlogController.java
+++ src/main/java/org/example/controller/BlogController.java
@@ -1,6 +1,8 @@
 package org.example.controller;
 
+import com.google.gson.Gson;
 import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
 import org.example.service.BlogService;
 import org.example.service.UserService;
 
@@ -11,7 +13,11 @@ public class BlogController {
         this.blogService = blogService;
     }
 
-    public void getAllBlogPosts(Context context) {
+    public void getAllBlogPosts(Context ctx) {
+        var blogs = blogService.getAllBlogPosts();
+        var json = gson.toJson(blogs);
+        ctx.status(HttpStatus.OK);
+        ctx.json(json);
     }
 
     public void saveBlogPost(Context context) {
